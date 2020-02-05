a506b0820426e516cd61fb76815d2acc8a690b81
Tjebbe <tjebbemarchand@icloud.com>
2020-02-05 10:19:44 +0100

    Clear text inputs with clear function

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
 
-    clear() {}
+    clear() {
+        this.previousOperand = "";
+        this.currentOperand = "";
+        this.operation = undefined;
+    }
 
     delete() {}
 
