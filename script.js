7b067a34b5383358d1edae1a0a42113b197b9e7b
Tjebbe <tjebbemarchand@icloud.com>
2020-02-05 10:18:22 +0100

    Created class for the calculator

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+
+    clear() {}
+
+    delete() {}
+
+    appendNumber(number) {}
+
+    chooseOperation(operation) {}
+
+    compute() {}
+
+    updateDisplay() {}
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
